3b6d57f4daf2eec18452431ab6524f7d0448d2d4
Ranga Nikitha <36513863+nikitha1999@users.noreply.github.com>
2026-09-21 19:08:29 -0400

    Adding Push Notifications Tool

--- PersonaAgent.py
+++ PersonaAgent.py
@@ -4,12 +4,18 @@ from openai import OpenAI
 from pypdf import PdfReader
 import gradio
 import json
+import requests
 
 # load or reload env
 load_dotenv(override=True)
 
 # read keys
 google_api_key = os.getenv('GOOGLE_API_KEY')
+pushover_user = os.getenv("PUSHOVER_USER")
+pushover_token = os.getenv("PUSHOVER_TOKEN")
+
+# push notifications Base URL
+pushover_url = "https://api.pushover.net/1/messages.json"
 
 # google Gemini Base URL
 gemini_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
@@ -40,7 +46,7 @@ systemPrompt = systemPrompt.replace("{summary}", summary)
 systemPrompt = systemPrompt.replace("{linkedin}", linkedinData)
 
 # write tools
-# as of now only email is the tool
+# email tool
 recordEmailJson = {
     "name": "record_email_tool",
     "description": "Use this tool to record that a user provided their email address",
@@ -57,11 +63,49 @@ recordEmailJson = {
     }
 }
 
+# push notification tool
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {"type": "string", "description": "The email address of this user"},
+            "name": {"type": "string", "description": "The user's name, if they provided it"},
+            "notes": {"type": "string", "description": "Any additional info about the conversation that's worth recording to give context"
+            }
+        },
+        "required": ["email"],
+        "additionalProperties": False
+    }
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {"type": "string", "description": "The question that couldn't be answered"},
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
+
 # create tools object
 tools = [
     {
         "type": "function",
         "function": recordEmailJson
+    },
+    {
+        "type": "function", 
+        "function": record_user_details_json
+    },
+    {
+        "type": "function", 
+        "function": record_unknown_question_json
     }
 ]
 
@@ -72,19 +116,43 @@ def record_email_tool(email):
         f.write(email + "\n")
     return "Email received"
 
+def push(message):
+    print(f"Push: {message}")
+    payload = {"user": pushover_user, "token": pushover_token, "message": message}
+    requests.post(pushover_url, data=payload)
+
+# define - record user details
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording interest from {name} with email {email} and notes {notes}")
+    return "OK"
+
+# define - record unknown questions
+def record_unknown_question(question):
+    push(f"Recording {question} asked that I couldn't answer")
+    return "OK"
+
+# tackle tools
+def handle_tool_calls(tool_calls):
+    results = []
+    for tool_call in tool_calls:
+        tool_name = tool_call.function.name
+        arguments = json.loads(tool_call.function.arguments)
+        print(f"Tool called: {tool_name}", flush=True)
+        tool = globals().get(tool_name)
+        result = tool(**arguments) if tool else "No tool found"
+        results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+    return results
+
 # define chat function
 def chat(message, history):
-    history = [{"role": h["role"], "content": h["content"]} for h in history]
     messages = [{"role": "system", "content": systemPrompt}] + history + [{"role": "user", "content": message}]
     response = google.chat.completions.create(model="gemini-3.5-flash-lite", messages = messages, tools= tools)
-
-    while response.choices[0].finish_reason == "tool_calls":
+    while response.choices[0].finish_reason=="tool_calls":
         message = response.choices[0].message
+        tool_calls = message.tool_calls
+        results = handle_tool_calls(tool_calls)
         messages.append(message)
-        for tool_call in message.tool_calls:
-            email = json.loads(tool_call.function.arguments).get("email")
-            record_email_tool(email)
-            messages.append({"role": "tool", "content": "Email recorded", "tool_call_id": tool_call.id})
+        messages.extend(results)
         response = google.chat.completions.create(model="gemini-3.5-flash-lite", messages=messages, tools=tools)
     return response.choices[0].message.content
 
